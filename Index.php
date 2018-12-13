1dfa4a87833c3d29dba2cb5ae89236683f2292a0
fredvunkiktkhk <fred.vunk@khk.ee>
2018-12-13 15:04:26 +0200

    Väljastus (Echo & print - ära kasuta)

---
+++ Index.php
@@ -0,0 +1,10 @@
+<?php
+//Teksti väljastus print käsuga
+print 'See on esimene lause PHP keeles'.'<br>';
+print 'See on teine lause PHP keeles'.'<br>';
+//teksti väljastus echo käsuga
+echo 'See tekst on echo abil väljastatud'
+
+
+
+?>
