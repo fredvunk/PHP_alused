06c28d7c8283a24499db19cc963ba185d042217e
fredvunkiktkhk <fred.vunk@khk.ee>
2018-12-13 15:25:11 +0200

    Variables

---
+++ Variables/index.php
@@ -0,0 +1,10 @@
+<?php
+//muutujate defineerimine
+// $muutujaNimi = väärtus;
+$taisArv = 7;
+$reaalArv = 3.14;
+$sone = 'kodu';
+$toevaartusTrue = true;
+$toevaartusFalse = false;
+
+echo 'Täisarv = '.$taisArv.'<br>';
