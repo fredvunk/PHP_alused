ee69d5b05e79929a421db3a1de666f7300925627
fredvunkiktkhk <fred.vunk@khk.ee>
2018-12-17 13:28:17 +0200

    Funktsioon mittekohustusliku argumendiga

--- functions/index.php
+++ functions/index.php
@@ -1,17 +1,17 @@
 <html>
 <head>
-    <title>Listing 6.11</title>
+    <title>Listing 6.12</title>
 </head>
 <body>
 <?php
-function fontWrap($txt, $size)
+function fontWrap($txt, $size=3)
 {
     print "<font size=\"$size\"face=\"Helvetica,Arial,Sans-serif\">$txt</font>";
 }
 fontWrap("A heading<br>",5);
-fontWrap("some body text<br>",3);
-fontWrap("some more body text<br>",3);
-fontWrap("yet more body text<br>",3);
+fontWrap("some body text<br>");
+fontWrap("some more body text<br>");
+fontWrap("yet more body text<br>");
 ?>
 </body>
 </html>
