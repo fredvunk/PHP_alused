2c2f132e4ba1dff2e3eb40848ad5122ea96d2f9b
fredvunkiktkhk <fred.vunk@khk.ee>
2018-12-17 13:06:40 +0200

    Funktsiooni väljakutsumine

--- functions/index.php
+++ functions/index.php
@@ -1,20 +1,12 @@
+<html>
+<head>
+    <title>Listing 6.1</title>
+</head>
+<body>
 <?php
-
-/*function funktsiooniNimi($param1, $param2, ...$paramN) { tegevused, mis toimuvad fn sees }*/
-
-
-
-function numbriKordumine($arv, $kontrollNumber) {
-    //leia, mitu korda siin esinemas number 5;
-    $numberKokku = 0;
-    while ($arv != 0) {
-        $number = $arv % 10;
-        if($number == $kontrollNumber) {
-            $numberKokku = $numberKokku +1; // $numberkokku++;
-        }
-        $arv = $arv / 10;
-        settype($arv, 'int');
-    }
-    echo 'Number '.$kontrollNumber.' esineb antud arvus '.$numberKokku.' korda<br>';
-}
-numbriKordumine(44258755745, 5);
+$num = - 321;
+$newnum = abs ($num);
+print $newnum;
+?>
+</body>
+</html>
