f250c41d9b7bfdd912414577a3fa243c3623b036
fredvunkiktkhk <fred.vunk@khk.ee>
2018-12-18 12:17:53 +0200

    Arrays + väljastamine & elementide töötlus

--- Arrays/index.php
+++ Arrays/index.php
@@ -5,8 +5,20 @@ $kasutajad[] = 'alice';
 $kasutajad[] = 'bob';
 array_push($kasutajad, 'lucy');
 $lisaKasutajad = array('mark', 'george');
-echo '<pre>';
-print_r($kasutajad);
-print_r($lisaKasutajad);
-echo '</pre>';
-//var_dump($kasutajad);
+
+// massiivi pikkus
+$kasutajadKokku = count($kasutajad);
+for($i = 0; $i < 3; $i++) {
+    echo $kasutajad[$i].'<br>';
+}
+
+array_shift($kasutajad);
+for($i = 0; $i < $kasutajadKokku; $i++) {
+    echo $kasutajad[$i].'<br>';
+}
+
+$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
+for($i = 0; $i < $uuedKasutajad; $i++) {
+    echo $uuedKasutajad[$i].'<br>';
+}
+
