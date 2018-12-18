af7e41afeb0b2421802ed7058e486f4c74b668ff
fredvunkiktkhk <fred.vunk@khk.ee>
2018-12-18 12:38:34 +0200

    Arrays + väljastamine värviliselt

--- Arrays/index.php
+++ Arrays/index.php
@@ -1,29 +1,26 @@
 <?php
-$kasutajad = array(); // tühi massiiv
-// andmete lisamine
-$kasutajad[] = 'alice';
-$kasutajad[] = 'bob';
-array_push($kasutajad, 'lucy');
-$lisaKasutajad = array('mark', 'george');
-
-// massiivi pikkus
-$kasutajadKokku = count($kasutajad);
-for($i = 0; $i < 3; $i++) {
-    echo $kasutajad[$i].'<br>';
-}
-
-array_shift($kasutajad);
-for($i = 0; $i < $kasutajadKokku; $i++) {
-    echo $kasutajad[$i].'<br>';
-}
-
-$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
-for($i = 0; $i < count($uuedKasutajad); $i++) {
-    echo $uuedKasutajad[$i].'<br>';
-}
-
-echo '<hr>';
-
-foreach ($uuedKasutajad as $kasutaja) {
-    echo $kasutaja.'<br>';
+$kasutajad = array(
+    array(
+        'alice',
+        'Alice',
+        'Liddel',
+        'female'
+    ),
+    array(
+        'bob',
+        'Bob',
+        'Builder',
+        'male'
+    )
+    );
+for($i = 0; $i < count($kasutajad); $i++){
+    foreach ($kasutajad[$i] as $info) {
+        if ($kasutajad[$i][3] == 'female') {
+            echo '<div style="color: red">';
+        } else {
+            echo '<div style="color: blue">';
+        }
+        echo $info . '</div>';
+    }
+    echo '<hr>';
 }
