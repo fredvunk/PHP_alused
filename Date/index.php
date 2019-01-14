401f0ba8080788c23fc742a1edd79bed9fda7b1a
fredvunkiktkhk <fred.vunk@khk.ee>
2019-01-14 10:35:00 +0200

    Tehted kuupäevadega

--- Date/index.php
+++ Date/index.php
@@ -14,3 +14,13 @@ echo 'Eestipärane kuupäev: '.$paev.'.'.$kuu.'.'.$aasta.'<br>';
 
 $a = mktime(0,0,0,10,22,1995-25);
 echo 'Muu soovitud kuupäeva genereerimine: '.date('d.m.Y', $a).'<br><br>';
+
+echo '1min pärast on kell: '.date('d.m.Y G:i', time() + 60).'<br>';
+echo '1hr pärast on kell: '.date('d.m.Y G:i', time() + 60*60).'<br>';
+echo '24hr pärast on kell: '.date('d.m.Y G:i', time() + 60*60*24).'<br><br>';
+
+$arv1 = 10;
+$arv2 = 54;
+$arv3 = 22;
+$aasta = 2018;
+$aasta2 = 2017;
