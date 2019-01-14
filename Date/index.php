f675d38fe7bf0bc8134edee3ac28959c3dd05f04
fredvunkiktkhk <fred.vunk@khk.ee>
2019-01-14 10:33:34 +0200

    Muu kuupäeva genereerimine

--- Date/index.php
+++ Date/index.php
@@ -11,3 +11,6 @@ $kuu = $eesti_kuud[date('n')];
 $aasta = date('Y');
 
 echo 'Eestipärane kuupäev: '.$paev.'.'.$kuu.'.'.$aasta.'<br>';
+
+$a = mktime(0,0,0,10,22,1995-25);
+echo 'Muu soovitud kuupäeva genereerimine: '.date('d.m.Y', $a).'<br><br>';
