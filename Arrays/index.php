a437cc8e977ab2806da37a378b6529a7d07ee7ca
fredvunkiktkhk <fred.vunk@khk.ee>
2019-01-14 10:26:53 +0200

    Aja loomine

--- Arrays/index.php
+++ Arrays/index.php
@@ -14,13 +14,20 @@ $kasutajad = array(
     )
     );
 for($i = 0; $i < count($kasutajad); $i++){
+    echo'<table border="2px black solid">';
     foreach ($kasutajad[$i] as $info) {
         if ($kasutajad[$i][3] == 'female') {
+            echo'<tr>';
             echo '<div style="color: red">';
+            echo'</tr>';
         } else {
+            echo'<tr>';
             echo '<div style="color: blue">';
         }
+        echo'</tr>';
         echo $info . '</div>';
+        echo'</tr>';
     }
+    echo'</table>';
     echo '<hr>';
 }
