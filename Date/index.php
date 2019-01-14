078aa195a75e2499350af3fa6db3033a5fc3faaf
fredvunkiktkhk <fred.vunk@khk.ee>
2019-01-14 10:32:48 +0200

    Eesti kuupäev

--- Date/index.php
+++ Date/index.php
@@ -3,3 +3,11 @@ echo time().'<br>';
 
 echo date('d.m.Y G:i' , time()).'<br>'; //22.02.2019 16:02
 echo 'Ajavöönd: '.date_default_timezone_get('Europe/Tallinn').'<br>';
+
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember' );
+
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+
+echo 'Eestipärane kuupäev: '.$paev.'.'.$kuu.'.'.$aasta.'<br>';
