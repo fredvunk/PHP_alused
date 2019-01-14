5440712de79ff71dbab44d0aa935f18f47fa3f26
fredvunkiktkhk <fred.vunk@khk.ee>
2019-01-14 10:31:24 +0200

    Ajavöönd

--- Date/index.php
+++ Date/index.php
@@ -1,4 +1,5 @@
 <?php
-echo time();
+echo time().'<br>';
 
-echo date('d.m.Y G:i' , time()); //22.02.2019 16:02
+echo date('d.m.Y G:i' , time()).'<br>'; //22.02.2019 16:02
+echo 'Ajavöönd: '.date_default_timezone_get('Europe/Tallinn').'<br>';
