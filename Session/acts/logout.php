9ef869853094dfbbf2ef4ac5db9a5c266ab6f794
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-01-27 11:30:35 +0200

    Session - mustand - error connecting server

---
+++ Session/acts/logout.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vunk7
+ * Date: 26-Jan-19
+ * Time: 13:55
+ */
---
+++ Session/db/index.php
@@ -0,0 +1,35 @@
+<?php
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//
+//// Create connection
+//$conn = new mysqli($servername, $username, $password);
+//
+//// Check connection
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
+//echo "Connected successfully";
+//?>
+//
+//require_once 'conf.php';
+//require_once 'db_fnk.php';
+//$iktConn = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
+//echo '<pre>';
+//print_r($iktConn);
+
+//function connect_db($hostName, $dbUser, $dbPass, $dbName) {
+//    $conn = mysqli_connect($hostName,$dbUser, $dbPass, $dbName);
+//    if(!$conn){
+//        echo 'unable to connect to MySQL<br>';
+//        echo mysqli_connect_errno().'<br>';
+<!--//        exit;-->
+//    }
+//    return $conn;
+//}
+////require_once 'conf.php';
+//$iktConn = connect_db('localhost','fredvunkiktkhk','J7q?Lpv26e9]','fredvunk_PHP');
+//echo '<pre>';
+//print_r($iktConn);
+//echo '</pre>';
