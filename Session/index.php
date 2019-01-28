9a2d7878889c169ec35c800c418780b454619112
fred.vunk <fred.vunk@khk.ee>
2019-01-28 13:04:32 +0200

    Session - loodud funktsioon vormi väljastamiseks

--- Session/index.php
+++ Session/index.php
@@ -1,33 +1,5 @@
 <?php
-//require_once 'conf.php';
-//require_once 'db_fnk.php';
-//$iktConn = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
-//echo '<pre>';
-//print_r($iktConn);
-//
+require_once 'fnk.php';
 
-
-
-//function connect_db($hostName, $dbUser, $dbPass, $dbName) {
-//    $conn = mysqli_connect($hostName,$dbUser, $dbPass, $dbName);
-//    if(!$conn){
-//        echo 'unable to connect to MySQL<br>';
-//        echo mysqli_connect_errno().'<br>';
-//        exit;
-//    }
-//    return $conn;
-//}
-//require_once 'conf.php';
-//$iktConn = connect_db('localhost','fredvunk_edgar','uusphptest12','fredvunk_PHP');
-//echo '<pre>';
-//print_r($iktConn);
-//echo '</pre>';
-
-//require_once 'conf.php';
-//require_once 'db_fnk.php';
-//
-//$iktConn = connect_db(DBHOST,DBUSER,DBPASS,DBNAME);
-//
-//echo '<pre>';
-//print_r($iktConn);
-//echo '</pre>';
+$loginForm = htmlFromFile('login');
+echo $loginForm;
