f6d0f730b7af8de1689fb1a6950bcb115385f7e2
fred.vunk <fred.vunk@khk.ee>
2019-01-28 12:44:43 +0200

    Andmebaasiga ühendamine + conf fail

---
+++ Session/db/conf.php
@@ -0,0 +1,5 @@
+<?php
+define('DBHOST', 'localhost:3306');
+define('DBUSER', 'fredvunk_edgar');
+define('DBPASS', 'uusphptest12');
+define('DBNAME', 'fredvunk_PHP');
---
+++ Session/db/index.php
@@ -0,0 +1,23 @@
+<?php
+
+//
+//require_once 'conf.php';
+//require_once 'db_fnk.php';
+//$iktConn = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
+//echo '<pre>';
+//print_r($iktConn);
+
+function connect_db($hostName, $dbUser, $dbPass, $dbName) {
+    $conn = mysqli_connect($hostName,$dbUser, $dbPass, $dbName);
+    if(!$conn){
+        echo 'unable to connect to MySQL<br>';
+        echo mysqli_connect_errno().'<br>';
+        exit;
+    }
+    return $conn;
+}
+require_once 'conf.php';
+$iktConn = connect_db(DBHOST,DBUSER,DBPASS,DBNAME);
+echo '<pre>';
+print_r($iktConn);
+echo '</pre>';
