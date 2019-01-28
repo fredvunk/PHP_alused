730d5b40eba4f14d12ef5df532f98070e9650dbf
fred.vunk <fred.vunk@khk.ee>
2019-01-28 13:21:43 +0200

    Session -  loodud kasutaja andmete kontroll andmebaasist

--- Session/acts/login.php
+++ Session/acts/login.php
@@ -1,5 +1,14 @@
 <?php
+
+require_once '../db/conf.php';
+require_once '../db/db_fnk.php';
 $username = $_GET['uname'];
 $password = $_GET['upass'];
 
-echo $username.' ',$password;
+// loome ühenduse andmebaasiga
+$ikt = connect_db(DBHOST,DBUSER,DBPASS,DBNAME);
+// küsime kasutaja andmete olemasolu andmebaasis
+$sql = 'SELECT * FROM fred_php WHERE username="'.$username.'" AND password="'.md5($password).'"';
+$users = getData($sql, $ikt);
+//väljastame andmed
+echo $users[0]['first_name'].' '.$users[0]['last_name'];
